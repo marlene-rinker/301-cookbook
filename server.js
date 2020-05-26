15ce0251adbc97fc11edac01ca09eeb828944c45
Davidoffili <palagashvilid@gmail.com>
2020-05-26 15:58:55 -0700

    homepage set, placeholders for partials set, packages loaded locally, SMACCS preset for stylesheets, scaffolded order of elements based on wireframe.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const superagent = require('superagent');
 const pg = require('pg');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const methodOverride = require('method-override');
 
@@ -13,15 +13,12 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('./public'));
 app.use(methodOverride('_overrideMethod'));
-const client = new pg.Client(process.env.DATABASE_URL);
-client.on('error', console.error);
-client.connect();
+// const client = new pg.Client(process.env.DATABASE_URL);
+// client.on('error', console.error);
+// client.connect();
 
-
-
-
-
-
-
+app.get('/', (request, response) => {
+    response.render('pages/index');
+});
 
 app.listen(PORT, console.log(`running on ${PORT}`));
