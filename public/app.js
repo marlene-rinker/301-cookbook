9d67369286ff4af767dc256ef886718ef7ef61b2
d-d-wolfe <dave.wolfe623@gmail.com>
2020-05-28 16:02:35 -0700

    completed styling the app

--- public/app.js
+++ public/app.js
@@ -7,42 +7,47 @@ function setEventListeners() {
 
 $('#message').hide();
 
-function saveIt(event){
+function saveIt(event) {
   event.preventDefault();
   let result = event.target;
   console.log(result.sourceurl.value);
-  $.post('/save', {title: result.title.value,
+  $.post('/save', {
+    title: result.title.value,
     image: result.image.value,
     sourceurl: result.sourceurl.value,
     readyinminutes: result.readyinminutes.value,
     servings: result.servings.value,
-    id: result.api_id.value },
-  function(){
-    $('#message').show();
-    $('#message').text(`${result.title.value} was saved to your Superdex.`);
-    setInterval(function(){$('#message').hide();}, 3000);
-  });
+    id: result.api_id.value
+  },
+    function () {
+      $('#message').show();
+      $('#message').text(`${result.title.value} was saved to your Superdex.`);
+      setInterval(function () { $('#message').hide(); }, 3000);
+    });
 }
 
-function deleteIt(event){
+function deleteIt(event) {
   event.preventDefault();
   let result = event.target;
-  $.post('/delete?_overrideMethod=DELETE', {id: result.id.value},
-    function(){
+  $.post('/delete?_overrideMethod=DELETE', { id: result.id.value },
+    function () {
       event.target.parentElement.remove();
       $('#message').show();
       $('#message').text(`${result.title.value} was deleted from your Superdex.`);
-      setInterval(function(){$('#message').hide();}, 3000);
+      setInterval(function () { $('#message').hide(); }, 3000);
     });
 }
 
 function myFunction() {
-    let x = document.getElementById("main-nav"); 
-    if (x.style.display === "grid") {
-        x.style.display = "none"; 
-    } else {
-        x.style.display = "grid"; 
-    }
+  console.log('function working')
+  let x = document.getElementById("main-nav");
+  if (x.style.display === "grid") {
+    x.style.display = "none";
+    console.log('display was grid')
+  } else {
+    x.style.display = "grid";
+    console.log('display was not grid')
+  }
 };
 
 
