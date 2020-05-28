b5edcbdd6d76e951c2c8b3234c5bc65c932a6ad9
MasonChance <mason.fryberger@gmail.com>
2020-05-28 11:24:56 -0700

    re-organized server file and updated .catches with errorHandler

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const superagent = require('superagent');
 const pg = require('pg');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 const app = express();
 const methodOverride = require('method-override');
 const cors = require('cors');
@@ -25,35 +25,42 @@ client.on('error', console.error);
 client.connect();
 
 
-app.get('/', (req, res) => res.render('pages/index'))
+//===Routes ====//
+app.get('/', showHome);
 
-//===!!!search route Querries API Spoontacular. ====//
-//=== the functions contained within will be later extracted into a module. 
 app.post('/search', searchResults)
 
 app.get('/show', (req, res) => res.render('show.ejs'))
+app.get('/my-superdex', showSaved);
+
+app.post('/save', saveRecipe);
+app.delete('/delete', deleteRecipe);
+
+app.get('/about', (request, response) => {
+  response.render('pages/about_us', {'bioArray': bios});
+});
+// app.get('/results', showSearchResults);
+
+//=== the functions contained within will be later extracted into a module. 
 
 //== Constructor for Recipe Object ==//
 // image size options 90x90, 240x150, 312x150
-// the img extension can be found at the end of the card.image value, if we need to make this dynamic we can use a function to pull the extention and pass it in to the template literal
- 
+
 function RecipeCard(apiObj){
- 
   this.title = apiObj.title;
   this.image = apiObj.image ? ` https://spoonacular.com/recipeImages/${this.api_id}-240x150.` : 'public/styles/imgs/alt-image-lorempixel.jpg';
   this.sourceUrl = apiObj.sourceUrl ? httpSecure(apiObj.sourceUrl) : 'We\'re sorry, that is unavailable at this time.';
   this.readyInMinutes = apiObj.readyInMinutes;
   this.servings = apiObj.servings;
-  this.foreignKey;
-  this.api_id = apiObj.id // this may be most useful if we open the option to search further details on a saved recipe, allowing us to make single api calls for each purpose instead of needing more than one. 
+  this.foreignKey; 
+  this.api_id = apiObj.id 
   
 }
 
 //== Constructor Method for Query Object ==//
 
 RecipeCard.Query = function (req, res){
-  
-  this.apiKey = process.env.SPOONTACULAR_API_KEY;
+  this.apiKey = process.env.SPOONACULAR_API_KEY;
   this.query = req.body.search;
   this.number = 1; // may be made dynamic if future patches allow filtering number of results. 
 }
@@ -61,7 +68,6 @@ RecipeCard.Query = function (req, res){
 
 //=== !!! Callback for app.post(/search)==//
 function searchResults(req, res){
-  // console.log('req.body@ searchResults:server.js-->', req.body)//[x] returns:{search : 'meatloaf'}
   const spoonUrl = 'https://api.spoonacular.com/recipes/search';
   const query = new RecipeCard.Query(req);
   
@@ -76,7 +82,6 @@ function searchResults(req, res){
 
 //== Runs response from api through constructor and returns: array of object literals. 
 function compileList(list){
-  //[x] console.log('list.body.results[0] @ compileList:server.js-->', list.body.results[0]);
   const menu = list.body.results.map(curr => new RecipeCard(curr))
   return menu; 
 }
@@ -86,37 +91,10 @@ function renderMenu(res, menu){
   // console.log('menu @renderMenu:server.js', menu)
   res.render('./pages/show', {'list' : menu , 'showResults': true});
 }
-//==!!! FUNCTION TO APPLY SECURED PROTOCOL TO URLS=== inputReq: url <string> !!!//
-//
-function httpSecure(url){
-  if(url.charAt(4) !== 's'){  
-   return `https${url.slice(4)}` 
-  } else {
-    return url;
-  }
- }
-
- //!!! == ERROR HANDLER !!! inputReq: error from .catch, req, res, and the correct path as a string. 
-function errorCatch(req, res, error, path){
-  res.render(`${path}`, {'error': error});
-}
 
 
 
-
-//routes
-app.get('/', showHome);
-app.post('/save', saveRecipe);
-app.delete('/delete', deleteRecipe);
-app.get('/my-superdex', showSaved);
-// app.get('/results', showSearchResults);
-app.get('/about', (request, response) => {
-  response.render('pages/about_us');
-});
-
-
-//functions
-
+//functions Renders Home page//
 function showHome(req, res) {
   res.render('pages/index.ejs');
 }
@@ -128,10 +106,7 @@ function saveRecipe(req, res) {
     .then(() => {
       res.send();
     })
-    .catch(error => {
-      // errorCatch(req, res, error, 'pages/error.ejs');
-      console.log(error);
-    })
+    .catch(error => errorCatch(req, res, error, 'pages/error.ejs'));
 }
 
 function deleteRecipe(req, res) {
@@ -141,12 +116,8 @@ function deleteRecipe(req, res) {
     .then(() => {
       res.send()
     })
-    .catch(error => {
-      // errorCatch(req, res, error, 'pages/error.ejs');
-      console.log(error);
-    })
+    .catch(error => .catch(error => errorCatch(req, res, error, 'pages/error.ejs')));
 }
-
 
 const marlene = { name: 'Marlene Rinker', image: 'styles/imgs/MRinker_photo.jpeg', bio: 'I’m a Full-stack JavaScript software developer with a background in instructional design, technical writing, QA, and accounting. I love collaborating with others to create a great customer experience. In my past roles, I helped customers though documentation, training, and testing. Now, as a software developer, I get to help customers by influencing how the software is built.' };
 
@@ -181,26 +152,34 @@ function randomBios() {
   return newArr;
 }
 
-
-
-
 function showSaved (req, res){
   const sqlQuery = 'SELECT * FROM recipes';
   client.query(sqlQuery)
     .then(resultFromSql => {
       console.log(resultFromSql.rows);
       res.render('pages/show',{list : resultFromSql.rows, showResults: false});
-    });
+    })
+    .catch(error => .catch(error => errorCatch(req, res, error, 'pages/error.ejs')));
 }
 
+//==!!! FUNCTION TO APPLY SECURED PROTOCOL TO URLS=== inputReq: url <string> !!!//
 
+function httpSecure(url){
+  if(url.charAt(4) !== 's'){  
+   return `https${url.slice(4)}` 
+  } else {
+    return url;
+  }
+ }
 
+ //!!! == ERROR HANDLER !!! inputReq: error from .catch, req, res, and the correct path as a string. 
+ function errorCatch(req, res, error, path){
+  res.render(`${path}`, {'error': error});
+}
 
-app.get('/about', (request, response) => {
-  response.render('pages/about_us', {'bioArray': bios});
-});
+const bios = randomBios();
+
 
 
 app.listen(PORT, console.log(`running on ${PORT}`));
 
-const bios = randomBios();
